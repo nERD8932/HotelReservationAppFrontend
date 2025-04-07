ba2c68e27adb77334c5eeec1ae39a668e5889079
nerd8932 <samir.amin8932@gmail.com>
2025-04-07 18:17:04 -0300

    Added a back button callback and added interpolations for animations

--- app/src/main/java/com/smu/mcda/hotelreservationapp/SearchResultsActivity.kt
+++ app/src/main/java/com/smu/mcda/hotelreservationapp/SearchResultsActivity.kt
@@ -1,8 +1,11 @@
 package com.smu.mcda.hotelreservationapp
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.addCallback
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import com.smu.mcda.hotelreservationapp.network.RetrofitClient
 import com.smu.mcda.hotelreservationapp.network.SearchRequest
 import com.smu.mcda.hotelreservationapp.ui.theme.HotelReservationAppTheme
@@ -19,9 +23,20 @@ import kotlinx.coroutines.launch
 
 
 class SearchResultsActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        onBackPressedDispatcher.addCallback(this) {
+            val options = ActivityOptions.makeCustomAnimation(
+                this@SearchResultsActivity,
+                R.anim.slide_out_bottom,
+                R.anim.slide_in_top,
+            )
+            finish()
+            val intent = Intent(this@SearchResultsActivity, MainActivity::class.java)
+            startActivity(intent, options.toBundle())
+        }
 
         val location = intent.getStringExtra("location")
         val startDate = intent.getStringExtra("startDate")
